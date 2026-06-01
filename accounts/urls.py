942a8abe9d50a3b00580a124e212172dbc483c81
arnursultan <badasspubgm>
2026-06-01 20:17:20 +0600

    Custom Permissions 2 Lesson

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import RegisterView, MeView
+from .views import RegisterView, MeView, UserListView, UserDetailView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('me/', MeView.as_view()),
+    path('users/', UserListView.as_view()),
+    path('users/<int:pk>/', UserDetailView.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
+
 from .serializers import RegisterSerializer, UserSerializer
+from .permissions import IsTeacherOrAdmin, IsOwnerOrAdmin
 
 User = get_user_model()
 
@@ -22,3 +24,18 @@ class MeView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsTeacherOrAdmin]
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrAdmin]
+
+    def get_object(self):
+        obj = super().get_object()
+        self.check_object_permissions(self.request, obj)
+        return obj
