c5b0489f8b248041b9dbe3ec8a2f462b60c58c2b
arnursultan <badasspubgm>
2026-06-25 19:28:51 +0600

    add jazzmin

--- core/settings.py
+++ core/settings.py
@@ -14,6 +14,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -145,3 +146,46 @@ EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Administration panel",
+    "site_header": "Library",
+    "site_brand": "Server 36-1B",
+    "welcome_sign": "Welcome to the Server 36-1B admin panel",
+    "copyright": "Geeks",
+    "search_model": ["auth.User", "auth.Group"],
+    "topmenu_links": [
+        {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
+        {"name": "Support", "url": "https://t.me/ar_nursultan", "new_window": True},
+        {"model": "auth.User"},
+    ],
+    "show_sidebar": True,
+    "changeform_format": "horizontal_tabs",
+}
+
+JAZZMIN_UI_TWEAKS = {
+    #  Light themes
+    # "theme" : "flatly",
+    # "theme" : "simplex",
+    # "theme" : "cerulean",
+    # "theme" : "cosmo",
+    # "theme" : "journal",
+    # "theme" : "litera",
+    # "theme" : "lumen",
+    # "theme" : "lux",
+    # "theme" : "materia",
+    # "theme" : "minty",
+    # "theme" : "pulse",
+    # "theme" : "sandstone",
+    # "theme" : "simplex",
+    # "theme" : "sketchy",
+    # "theme" : "spacelab",
+    # "theme" : "united",
+    "theme" : "yeti",
+    #  Dark themes
+    # "theme": "darkly",
+    # "theme" : "slate",
+    # "theme" : "cyborg",
+    # "theme" : "solar",
+    # "theme" : "superhero",
+}
